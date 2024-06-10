2cc957506efcda8ecb5319a8c520cbea0740ae7e
ARNAV SHARDA <146191610+arnav7897@users.noreply.github.com>
2024-06-10 23:35:23 +0530

    Update programme1.cpp

--- if- else in cpp/programme1.cpp
+++ if- else in cpp/programme1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>> a;
+//  if  - else if - else  work as joint statement 
+    //  why we require if, else if ,else ?
+        //  in order to connect a statement or a problem with 1 or various condition on same variables
+    
 
+
+
+
+    if(a>25){
+        cout<<"hi a > 25"<<endl;
+    }
+
+    else if(a>25 && a<65){
+        cout<<"a <25"<<endl;
+    }
+
+    else{
+        cout<<"bye"<<endl;
+    }
+}
